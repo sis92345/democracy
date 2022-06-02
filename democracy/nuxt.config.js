aa64db695fc971e02a4e1c485853a678436a298a
sis92345 <anjfqhk1014@naver.com>
2022-06-02 21:42:40 +0900

    [프로젝트 공통 설정] nuxt generate 경로 설 및 명령어 수정

--- democracy/nuxt.config.js
+++ democracy/nuxt.config.js
@@ -43,5 +43,10 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
-  }
+  },
+
+  // GENERATE시 Build될 경로
+  generate : {
+    dir : "../src/main/resources/static",
+  },
 }
