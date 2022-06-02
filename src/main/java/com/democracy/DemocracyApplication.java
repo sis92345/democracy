540b7e94a8319250e7fb340c2db8698b0339c624
sis92345 <anjfqhk1014@naver.com>
2022-06-02 19:46:08 +0900

    최초 생성

---
+++ src/main/java/com/democracy/DemocracyApplication.java
@@ -0,0 +1,13 @@
+package com.democracy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocracyApplication {
+		
+		public static void main( String[] args ) {
+				SpringApplication.run( DemocracyApplication.class, args );
+		}
+		
+}
---
+++ src/test/java/com/democracy/DemocracyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.democracy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocracyApplicationTests {
+		
+		@Test
+		void contextLoads() {
+		}
+		
+}
