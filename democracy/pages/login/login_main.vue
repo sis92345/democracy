f4fe645018e5b81a2cb859dcd89c6c39291d59f9
sis92345 <anjfqhk1014@naver.com>
2022-06-05 03:05:54 +0900

    [사용자] 로그인 > 로그인 퍼블리싱 기초 완료

--- democracy/pages/login/login_main.vue
+++ democracy/pages/login/login_main.vue
@@ -1,18 +1,22 @@
 <template>
   <div  class="login_main_container">
     <div class="login_box">
-      <img src="@/assets/image/logo/democracy.png"  alt="logo"/>
+      <img src="@/assets/image/logo/democracy2.png"  alt="logo"/>
       <div class="login_box_id_pw">
-        <div class="inner-contents">
+        <div class="inner-contents mtb-05">
           <el-input v-model="id"
                     prefix-icon="el-icon-user"
                     placeholder="아이디"/>
         </div>
-        <div class="inner-contents">
+        <div class="inner-contents mtb-05">
           <el-input v-model="pw"
                     prefix-icon="el-icon-lock"
                     placeholder="비밀번호"
                     show-password/>
+        </div>
+        <div class="mtb-2">
+          <el-button type="primary">로그인</el-button>
+          <el-button type="info">회원가입</el-button>
         </div>
       </div>
     </div>
