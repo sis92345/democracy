f4e6a3e00429740f6d1eafef66a7b302df957331
Ted <anjfqhk1014@naver.com>
2022-06-11 16:23:53 +0900

    [사용자] AppConfig 추가

---
+++ src/main/java/com/democracy/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.democracy.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories( basePackages = "com.democracy")
+public class AppConfig {
+}
