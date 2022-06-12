004226fb7bcdf2fcbcd3c466d91251ec9bc0f359
Ted <anjfqhk1014@naver.com>
2022-06-12 11:16:42 +0900

    [공통설정] Assertion Test

--- src/test/java/com/democracy/JPAConnectionTest.java
+++ src/test/java/com/democracy/JPAConnectionTest.java
@@ -24,7 +24,19 @@ class JPAConnectionTest extends StandardSpringTest {
 				
 				System.out.println( "JPA로 가져온 DB 데이터!!!!" + testData.getUSER_NAME() );
 				
-				Assertions.assertEquals( testData.getUSER_NAME() , "AN" );
+				Assertions.assertEquals( testData.getUSER_NAME() , "AN" , ()->{
+						
+						if ( testDataList.size() <= 0 ) {
+								
+								return "테스트 데이터가 비어있습니다. DB CONNECTION이 정상이라면 JPA 설정에 문제가 존재합니다.";
+						}
+						else if ( !testData.getUSER_NAME().equals( "AN" ) ) {
+								return "DB 값이 바뀌어서 실패한 경우 입니다. 정상적인 결과입니다. \n값 : " + testData.getUSER_NAME() ;
+						}
+						
+						
+						return  "";
+				});
 				
 		}
 }
