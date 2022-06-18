47258ddbe58c355221ecac9c3ace3acb14601d41
Ted <anjfqhk1014@naver.com>
2022-06-18 16:42:23 +0900

    <remove> : Test Service 추가

--- src/test/java/com/democracy/JPAConnectionTest.java
+++ src/test/java/com/democracy/JPAConnectionTest.java
@@ -4,9 +4,6 @@ import com.democracy.config.StandardSpringTest;
 import com.democracy.member.service.MemberRepository;
 import com.democracy.member.dto.Member;
 import com.democracy.member.service.MemberService;
-import com.democracy.sample.test.TestData;
-import com.democracy.sample.test.TestRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,39 +14,9 @@ import java.util.List;
 class JPAConnectionTest extends StandardSpringTest {
 		
 		@Autowired
-		TestRepository testRepository;
-		
-		@Autowired
 		MemberRepository memberRepository;
 		
 		@Autowired MemberService memberService;
-		
-		
-		
-		@Test
-		@DisplayName( "JPA 연결이 잘 되는지 테스트" )
-		public void JPA_CONNECTION_TEST() {
-				
-				List<TestData> testDataList = testRepository.findAll();
-				TestData testData = testDataList.get( 0 );
-				
-				System.out.println( "JPA로 가져온 DB 데이터!!!!" + testData.getUserName() );
-				
-				Assertions.assertEquals( testData.getUserName() , "AN" , ()->{
-						
-						if ( testDataList.size() <= 0 ) {
-								
-								return "테스트 데이터가 비어있습니다. DB CONNECTION이 정상이라면 JPA 설정에 문제가 존재합니다.";
-						}
-						else if ( !testData.getUserName().equals( "AN" ) ) {
-								return "DB 값이 바뀌어서 실패한 경우 입니다. 정상적인 결과입니다. \n값 : " + testData.getUserName() ;
-						}
-						
-						
-						return  "";
-				});
-				
-		}
 		
 		@Test
 		@DisplayName( "member 테이블 저장 테스트" )
