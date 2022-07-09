6cc918a66f6cc981c3a356b6de00862ac98dc916
hojin <sungsill150@gmail.com>
2022-07-09 16:40:47 +0900

    Add quasar structure in nuxt3 넉스트에 대한 구조 확립 및 설정 동기화

--- democracy_nuxt/nuxt.config.ts
+++ democracy_nuxt/nuxt.config.ts
@@ -1,5 +1,29 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
+//import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-})
+    // ssr: false,
+    css: [
+        "~/assets/styles/quasar.sass",
+        "@quasar/extras/roboto-font/roboto-font.css",
+        "@quasar/extras/material-icons/material-icons.css",
+    ],
+    build: {
+        transpile: ["quasar"],
+    },
+    vite: {
+        //define: {
+        //  __QUASAR_SSR__: true,
+        //  __QUASAR_SSR_SERVER__: true
+        //},
+        plugins: [
+            //vue({
+            // template: { transformAssetUrls },
+            //}),
+            quasar({
+                sassVariables: "assets/styles/quasar.variables.sass",
+            }),
+        ],
+    },
+});
