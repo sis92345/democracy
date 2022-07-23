107cf7e7d3dc5958353fd2b5106b47729d69c8bc
hojin <sungsill150@gmail.com>
2022-07-23 23:48:18 +0900

    Add Security config and filter
    JWT를 만들기 위해 사용할 것들 커밋

---
+++ src/main/java/com/democracy/jwt/JwtUtils.java
@@ -0,0 +1,89 @@
+package com.democracy.jwt;
+
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Configuration
+public class JwtUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+
+    @Value("${local.app.jwt.secret}")
+    private String jwtSecret;
+
+
+    @Value("${local.app.jwt.expire-timeout}")
+    private long accessExpiration;
+
+    @Value("${local.app.jwt.refresh-timeout}")
+    private long refreshExpiration;
+
+
+    @PostConstruct
+    protected void init(){
+        jwtSecret = Base64.getEncoder().encodeToString(jwtSecret.getBytes());
+    }
+
+    public String generateAccessToken(String userId) {
+        Long tokenInvalidTime = accessExpiration * 60 * 1000L;
+        return createToken(userId, tokenInvalidTime);
+    }
+
+    public String generateRefreshToken(String userId) {
+        Long tokenInvalidTime = refreshExpiration * 60 * 1000L;
+        String refreshToken = createToken(userId, tokenInvalidTime);
+//        redisService.setValues(refreshToken, userId, Duration.ofMillis(tokenInvalidTime));
+        return refreshToken;
+    }
+
+    public String createToken(String userId, Long tokenInvalidTime) {
+        return Jwts.builder().setSubject(userId).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + tokenInvalidTime))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+
+
+//    public Boolean checkRefreshToken(String refreshToken) {
+//        String redisRT = redisService.getValues(refreshToken);
+//        return redisRT == null ? false : true;
+//    }
+//
+//    public void deleteRefreshToken(String refreshToken) {
+//        redisService.deleteValues(refreshToken);
+//    }
+
+
+
+
+}
---
+++ src/main/java/com/democracy/member/service/UserService.java
@@ -0,0 +1,6 @@
+package com.democracy.member.service;
+
+public interface UserService {
+
+    //구현 해야 함
+}
