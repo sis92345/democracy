ff6a1eadc4d82dbd889d5e0501eb10a90875abb7
Ted <anjfqhk1014@naver.com>
2022-07-23 17:31:32 +0900

    [featrue][CONFIG] add eslint configuration
    add eslint configuration

--- front/plugins/axios.ts
+++ front/plugins/axios.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 export default defineNuxtPlugin(nuxtApp => {
     const domain = 'http://localhost:8080';
 
-    const serverApi = axios.create({
-        baseURL : domain
-    });
+    const serverApi = axios.create();
 
     return {
         provide: {
