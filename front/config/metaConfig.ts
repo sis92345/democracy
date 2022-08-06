1959885f9508bce1e477774f6874f56281d50d4d
Ted <anjfqhk1014@naver.com>
2022-08-06 15:08:43 +0900

    Favicon 추가

---
+++ front/config/metaConfig.ts
@@ -0,0 +1,37 @@
+/**
+ * Democracy Meta 정보 세팅
+ *
+ * @return {nuxtMetaObject} - nuxt Meta 정보
+ * */
+const getMetaSetting = function() : object {
+
+    console.log( "======= START META SETTING =======" );
+    /**
+     * @typedef nuxtMetaObject
+     * */
+    return {
+        head : {
+            title : process.env.SITE_NAME || 'Democracy',
+            meta : [
+                { charset : "utf-8" },
+                { "http-equiv" : "X-UA-Compatible", content : "IE=edge,chrome=1" },
+                { "http-equiv" : "pragma", content : "no-cache" },
+                { "http-equiv" : "cache-control", content : "no-cache" },
+                { "http-equiv" : "expires", content : "0" },
+                { name : "robots" , content : "noindex" },
+                { name : "viewport", content : "initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" },
+                {
+                    hid     : "description",
+                    name    : "description",
+                    content : "",
+                }
+            ],
+            link : [
+                { rel : "icon", type : "image/x-icon", href : "/favicon.ico" },
+            ],
+        },
+    }
+
+}
+
+export { getMetaSetting };
