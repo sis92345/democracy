d76ac9e71d819766cce951104eaea5cea0b03173
hojin <sungsill150@gmail.com>
2022-08-07 15:56:32 +0900

    edit DB Host change rds 호스트 아이피 변경으로 인한 커밋

--- src/main/java/com/democracy/member/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/democracy/member/service/impl/CustomUserDetailsServiceImpl.java
@@ -3,7 +3,6 @@ package com.democracy.member.service.impl;
 import com.democracy.member.dto.User;
 import com.democracy.member.service.CustomUserDetailsService;
 import com.democracy.member.service.JwtUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,8 +12,11 @@ import java.util.Optional;
 @Service
 public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
 
-    @Autowired
-    private JwtUserRepository jwtUserRepository;
+    private final JwtUserRepository jwtUserRepository;
+
+    public CustomUserDetailsServiceImpl(JwtUserRepository jwtUserRepository) {
+        this.jwtUserRepository = jwtUserRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
